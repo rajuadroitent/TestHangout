fafb0214359ca6f15ea929558358bac8211a268b
rajuadroitent <raju.bandari@adroitent.com>
2017-03-07 23:09:53 +0530

    devise gem added

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  root :to => 'home#index'
+  devise_for :customers
+
   namespace :customer do
     get 'customer_dashboard/index'
   end
 
-  devise_for :customers
   ActiveAdmin.routes(self)
   get 'sign_up/create'
   get 'sign_up/get_auth_code'
 
-    root :to => 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
